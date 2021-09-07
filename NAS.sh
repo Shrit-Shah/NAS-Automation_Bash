eb7ebb07af9eab0158d240e47bf4bfa158eac833
Shrit Shah <shritshah2113@gmail.com>
2021-09-08 01:46:53 +0530

    Added support for Raspberry-Pi

--- NAS.sh
+++ NAS.sh
@@ -1,27 +1,15 @@
 #!/bin/bash
 
-figlet NAS Automation 
-echo -e "   By:\t\tShrit Shah\tHarshil Shah\tNisarg Khacharia"
-#$client_ip=$(hostname -I | awk {'print $1}') # Client Private IP-address
-#$client_ip=$(dig +short myip.opendns.com @resolver1.opendns.com) # Client Public IP-address
+clear
+which figlet &>> /dev/null
+if [ $? -eq 0 ]
+then 
+    figlet NAS Automation 
+    echo -e "   By:\t\tShrit Shah\tHarshil Shah\tNisarg Khacharia"
+else
+    echo -e "\v\v \t\t\t\t NAS AUTOMATION \n"
+fi
 
-#read server_ip  # Geting Server IP-address from user
-
-#ssh -i 
-# Server Side (OS which is going to store the data)
-
-#yum install nfs-utils  # NAS software installation
-#systemctl enable --now nfs-server # Starting the NFS service
-
-#mkdir $server_bak_dir  # Create backup folder
-
-#cat $server_bak_dir $client_ip (rw,no_root_squash) >> /etc/exports
-
-# Client Side (This OS)
-
-#mkdir $client_dir
-
-#mount $server_ip:$server_bak_dir $client_dir
 
 new_setup()
 {
@@ -36,6 +24,7 @@ new_setup()
         # IP validation - REGEX: ((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.|$)){4}
 
         echo -e "\nEstablishing connection to $server_ip ... \n"
+
         ping -c 3 $server_ip &>> /dev/null
         if [ $? -eq 0 ]
         then 
@@ -46,19 +35,25 @@ new_setup()
             if [ $? -eq 0 ]
             then
                 echo "SSH connection successful"
+                
                 read -p "Name of backup folder on the server: " server_bak_dir
                 cmd=$(echo sudo -S -p "Enter\ sudo\ password\ of\ server-side: " bash /tmp/server.sh ${usr_name} ${server_bak_dir} ${client_ip})
                 echo "Configuring NAS server on $server_ip ..."
                 ssh ${usr_name}@${server_ip} $cmd
                 if [ $? -eq 0 ]
                 then   
-                    echo "NAS Server configuration successful"
-                    read -p "Name of backup folder here on the client: " client_dir
+                    echo -e "\nServer configuration successful\n"
+                    read -p "Name of backup folder here on the CLIENT: " client_dir
                     mkdir ${HOME}/Desktop/${client_dir}
                     
-                    sudo mount  ${server_ip}:/home/${usr_name}/${server_bak_dir}  ${HOME}/Desktop/${client_dir} #Mounting directories
+                    sudo mount  ${server_ip}:/home/${usr_name}/Desktop/${server_bak_dir}  ${HOME}/Desktop/${client_dir} #Mounting directories
+                    if [ $? -eq 0 ]
+                    then    
+                        echo -e "\v\tSetup Successful"
+                        exit
+                    fi
                 else
-                    echo "NAS Server configuration failed"
+                    echo "Server configuration failed"
                 fi
             else
                 echo -e "SSH connection failed\nPlease run the below commands manually on the server system & run this script again."
@@ -67,18 +62,33 @@ new_setup()
         else
             echo "Connection Failed"
         fi
-    fi
 
-    if [ $server_location -eq 2 ]
+    elif [ $server_location -eq 2 ]
     then
         echo "Coming Soon!!"
+        #$client_ip=$(dig +short myip.opendns.com @resolver1.opendns.com) # Client Public IP-address
+    
+    else
+        echo -e "\vInvalid Input"
     fi
+
+
+}
+
+modify_setup()
+{
+    echo "Coming Soon!!"
+}
+
+uninstall()
+{
+    echo "Coming Soon!!"
 }
 
 while [ 0 ]
 do
     echo "-----------------------------------------------------------------------------"
-    echo -e "\v\t1) Setup new storage \n\t2) Modify existing configuration \n\t00) Exit" #Main Menu
+    echo -e "\v\t1) Setup new storage \n\t2) Modify existing configuration \n\t3) Remove all NAS connections \n\t00) Exit" #Main Menu
 
     read -p "--> " menu_opt
 
@@ -86,15 +96,20 @@ do
         1) 
             new_setup
             ;;
-        2) echo "Coming Soon!!"
+        2) 
+            modify_setup
             ;;
-        00) echo "Exiting"
+        3) 
+            uninstall
+            ;;
+        00) 
+            echo "Exiting"
             break
             ;;
         *)
             echo "Select valid option from the menu"
             ;;
     esac
-
 done
-exit 0
+
+exit 0 &>> /dev/null
--- server.sh
+++ server.sh
@@ -4,17 +4,36 @@ usr_name=$1
 server_bak_dir=$2
 client_ip=$3
 
-rpm -q nfs-utils >> /dev/null
-if [ $? -eq 1 ]
+which yum &>> /dev/null
+if [ $? -eq 0 ]
 then
-    yum install nfs-utils  # NAS software installation
-    systemctl enable --now nfs-server # Starting the NFS service
+    yum list --installed nfs-utils &>> /dev/null
+    if [ $? -ne 0 ]   # Not equal to zero
+    then
+        yum install nfs-utils -y # NAS software installation
+    fi
+        systemctl enable --now nfs-server # Starting the NFS service
+
 fi
-mkdir /home/${usr_name}/${server_bak_dir}  # Create backup folder
-chmod 777 /home/${usr_name}/${server_bak_dir}
 
-echo "/home/${usr_name}/${server_bak_dir} *(rw,no_root_squash)" | cat >> /etc/exports
+which apt &>> /dev/null     # Raspbian
+if [ $? -eq 0 ]
+then
+    apt list --installed nfs-kernel-server 2>> /dev/null | grep nfs-kernel-server &>> /dev/null
+    if [ $? -ne 0 ]
+    then
+        apt install nfs-kernel-server -y
+    fi
+        systemctl enable --now nfs-server # Starting the NFS service
+    
+fi
 
-systemctl restart nfs-server
+mkdir /home/${usr_name}/Desktop/${server_bak_dir}  # Create server backup folder
+chmod 777 /home/${usr_name}/Desktop/${server_bak_dir}
 
-exit 0
+echo "/home/${usr_name}/Desktop/${server_bak_dir} *(rw,no_root_squash)" | cat > /etc/exports
+
+systemctl restart nfs-server # Restarting NAS Server
+
+
+exit
